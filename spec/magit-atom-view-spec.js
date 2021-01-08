ba6df24f0549eb11b383a62a5507eb0dfdb94ddf
Seunguk Shin <seunguk.shin@gmail.com>
2021-01-08 23:02:08 +0900

    Add status function

---
+++ spec/magit-atom-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import MagitAtomView from '../lib/magit-atom-view';
+
+describe('MagitAtomView', () => {
+	it('has one valid test', () => {
+		expect('life').toBe('easy');
+	});
+});
